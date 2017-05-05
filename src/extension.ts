e8bb249d1f800c1f13bc776a68147930145e9ff0
Peter Fern <github@obfusc8.org>
2017-05-05 17:07:06 +1000

    Trigger toggle asynchronously, with a slight delay
    Avoids accidental text selection when buffer position changes during
    mouse-click.

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,14 @@ import { TextEditorSelectionChangeKind } from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 	if (vscode.workspace.getConfiguration("vtools").autoHideSideBar) {
-		vscode.window.onDidChangeTextEditorSelection(selection=> {
+		vscode.window.onDidChangeTextEditorSelection(selection => {
 			if (selection.kind != TextEditorSelectionChangeKind.Mouse || selection.selections.length == 0) return;
-			if (selection.selections.find(a=>a.isEmpty) == null) return;
-			//vscode.commands.executeCommand("workbench.files.action.focusFilesExplorer");
-			vscode.commands.executeCommand("workbench.extensions.action.showInstalledExtensions");
-			vscode.commands.executeCommand("workbench.action.toggleSidebarVisibility");
+			if (selection.selections.find(a => a.isEmpty) == null) return;
+			setTimeout(function () {
+				//vscode.commands.executeCommand("workbench.files.action.focusFilesExplorer");
+				vscode.commands.executeCommand("workbench.extensions.action.showInstalledExtensions");
+				vscode.commands.executeCommand("workbench.action.toggleSidebarVisibility");
+			}, 150);
 		});
 	}
 }
