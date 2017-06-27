23fb0b9df994bf14c3f57513f0e90f0af2fdc43a
Jordan Brandt <jordan.brandt@garmin.com>
2017-06-27 10:30:01 -0700

    * * Added "auto-hide output" setting.

--- src/extension.ts
+++ src/extension.ts
@@ -26,6 +26,28 @@ export function activate(context: vscode.ExtensionContext) {
 			lastSelectionTime = thisSelectionTime;
 		});
 	}
+
+	if (vscode.workspace.getConfiguration("vtools").autoHideOutput) {
+		vscode.window.onDidChangeTextEditorSelection(selection=> {
+			// if selection was not from a click, or if there are no selections, return
+			if (selection.kind != TextEditorSelectionChangeKind.Mouse || selection.selections.length == 0) return;
+
+			// if at least one editor's selection is a single-position (ie. not a segment of text), perform the side-bar hiding
+			var singlePos = selection.selections.find(a=>a.isEmpty) != null;
+			var thisSelectionTime = Date.now();
+			if (singlePos) {
+				setTimeout(function () {
+					// if the selection changed during the delay-time, cancel the side-bar hiding
+					if (lastSelectionTime != thisSelectionTime) return;
+
+					//vscode.commands.executeCommand("workbench.files.action.focusFilesExplorer");
+					vscode.commands.executeCommand("workbench.action.terminal.focus");
+					vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
+				}, vscode.workspace.getConfiguration("vtools").autoHideDelay);
+			}
+			lastSelectionTime = thisSelectionTime;
+		});
+	}
 }
 export function deactivate() {
 }
